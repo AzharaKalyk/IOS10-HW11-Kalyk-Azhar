4515c58a6e9e715c83da19cb428d9284eb8443f6
AzharaKalyk <kab_180495@mail.ru>
2023-06-15 14:14:43 +0600

    Initial Commit

---
+++ IOS10-HW11-Kalyk Azhar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS10-HW11-Kalyk Azhar
+//
+//  Created by Azhar Kalyk on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
